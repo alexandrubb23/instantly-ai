6d29d49adc7b4f9f2008a37200eef1c20965b7a7
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2025-08-28 19:54:03 +0300

    Validate create email

--- backend/src/routes/emails.js
+++ backend/src/routes/emails.js
@@ -1,4 +1,14 @@
 import { emailRepository } from "../repositories/email.repository.js";
+import z from 'zod'
+
+// This can be shared given that is a monorepo
+const schema = z.object({
+  to: z.email(),
+  cc: z.union([z.literal(""), z.email(), z.null()]),
+  bcc: z.union([z.literal(""), z.email(), z.null()]),
+  subject: z.string().min(2).max(100),
+  body: z.string().min(10).max(1000),
+});
 
 const plugin = async (fastify) => {
   fastify.get("/", async (request, reply) => {
@@ -15,6 +25,11 @@ const plugin = async (fastify) => {
 
   fastify.post("/", async (request, reply) => {
     const emailData = request.body;
+
+    const parseResult = schema.safeParse(emailData);
+    if (!parseResult.success) {
+      return reply.status(400).send({ error: z.treeifyError(parseResult.error) });
+    }
 
     const [email] = await emailRepository.create(emailData);
     reply.status(201).send(email);
